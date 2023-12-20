b1f983dd50ba071388b08ebbf78d215fa1b22d54
Irene Escalona Calleja <iescalon@c3r5s5.42urduliz.com>
2023-12-20 13:41:24 +0100

    ex23

---
+++ ex23/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 13:23:21 by iescalon          #+#    #+#             */
+/*   Updated: 2023/12/20 13:23:58 by iescalon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int main(void)
+{
+	t_point	point;
+	set_point(&point);
+	return (0);
+}
