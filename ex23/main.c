8e271c667f0b2588ac04630ee2a44053348b8ff5
Irene Escalona Calleja <iescalon@c3r5s5.42urduliz.com>
2023-12-21 10:15:37 +0100

    ex23

--- ex23/main.c
+++ ex23/main.c
@@ -6,11 +6,12 @@
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 13:23:21 by iescalon          #+#    #+#             */
-/*   Updated: 2023/12/20 13:23:58 by iescalon         ###   ########.fr       */
+/*   Updated: 2023/12/21 10:15:07 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_point.h"
+// #include <stdio.h>
 
 void	set_point(t_point *point)
 {
@@ -18,9 +19,11 @@ void	set_point(t_point *point)
 	point->y = 21;
 }
 
-int main(void)
+int	main(void)
 {
 	t_point	point;
+//	printf("%d\n", point.x);
+//	printf("%d", point.y);
 	set_point(&point);
 	return (0);
 }
